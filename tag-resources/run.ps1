84662c6b865d80e26c6eb296d6bf6a7ad062f01a
Justin OConnor <jsoconno@gmail.com>
2021-09-22 11:19:09 -0400

    add function directly to file

--- tag-resources/run.ps1
+++ tag-resources/run.ps1
@@ -1,6 +1,35 @@
-. .\Get-ParentResourceId.ps1
-
 param($eventGridEvent, $TriggerMetadata)
+
+function Get-ParentResourceId {
+    param(
+        $ResourceID
+    )
+
+    $ResourceIDList = $ResourceID -Split '/'
+    $IgnoreList = @('subscriptions', 'resourceGroups', 'providers')
+
+    for ($ia=$ResourceIDList.length-1; $ia -ge 0; $ia--) {
+        $CurrentResourceIDList = $ResourceIDList[0..($ia)]
+        $CurrentResourceID = $CurrentResourceIDList -Join '/'
+        $CurrentHead = $CurrentResourceIDList[-1]
+        if (!($IgnoreList -Contains $CurrentHead)) {
+            Write-Host "Trying to get tags for $($CurrentResourceID)" 
+            try {
+                $value = Get-AzTag -ResourceId $CurrentResourceID -ErrorAction silentlycontinue
+                if ($value -ne $Null) {
+                    Write-Host "Found tags for resource $($CurrentResourceID)"
+                    Break
+                }
+            } catch {
+                Write-Host "$($CurrentResourceID) cannot be tagged.  Searching for parent."
+            }
+        } else {
+            Write-Host "Skipping $($CurrentResourceID)"
+        }
+    }
+
+    Return $CurrentResourceID
+}
 
 $caller = $eventGridEvent.data.claims.name
 $lastOperation = $eventGridEvent.data.operationName
