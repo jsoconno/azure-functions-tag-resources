f70e2f2861e047184ddb3cb410318ce1aeaac9eb
Justin OConnor <jsoconno@gmail.com>
2021-09-22 10:54:34 -0400

    test function in file

--- tag-resources/run.ps1
+++ tag-resources/run.ps1
@@ -1,5 +1,34 @@
 #Import function
-. ..\get-parent-resource-id.ps1
+function Get-ParentResourceId {
+    param(
+        $ResourceID
+    )
+
+    $ResourceIDList = $ResourceID -Split '/'
+    $IgnoreList = @('subscriptions', 'resourceGroups', 'providers')
+
+    for ($ia=$ResourceIDList.length-1; $ia -ge 0; $ia--) {
+        $CurrentResourceIDList = $ResourceIDList[0..($ia)]
+        $CurrentResourceID = $CurrentResourceIDList -Join '/'
+        $CurrentHead = $CurrentResourceIDList[-1]
+        if (!($IgnoreList -Contains $CurrentHead)) {
+            Write-Host "Trying to get tags for $($CurrentResourceID)" 
+            try {
+                $value = Get-AzTag -ResourceId $CurrentResourceID -ErrorAction silentlycontinue
+                if ($value -ne $Null) {
+                    Write-Host "Found tags for resource $($CurrentResourceID)"
+                    Break
+                }
+            } catch {
+                Write-Host "$($CurrentResourceID) cannot be tagged.  Searching for parent."
+            }
+        } else {
+            Write-Host "Skipping $($CurrentResourceID)"
+        }
+    }
+
+    Return $CurrentResourceID
+}
 
 param($eventGridEvent, $TriggerMetadata)
 
