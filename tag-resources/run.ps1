90cb5355c5293121d098c5b1882aee5db1529161
Justin OConnor <jsoconno@gmail.com>
2021-09-23 16:06:50 -0400

    adding back old functionality and changing ignore filter

--- tag-resources/run.ps1
+++ tag-resources/run.ps1
@@ -77,63 +77,63 @@ function Get-ParentResourceId {
     Return $CurrentResourceID
 }
 
-$caller = $eventGridEvent.data.claims.name
-$lastOperation = $eventGridEvent.data.authorization.action
+$Requestor = $eventGridEvent.data.claims.name
+$Action = $eventGridEvent.data.authorization.action
 
-if ($null -eq $caller) {
+if ($null -eq $Requestor) {
     if ($eventGridEvent.data.authorization.evidence.principalType -eq "ServicePrincipal") {
-        $caller = (Get-AzADServicePrincipal -ObjectId $eventGridEvent.data.authorization.evidence.principalId).DisplayName
-        if ($null -eq $caller) {
+        $Requestor = (Get-AzADServicePrincipal -ObjectId $eventGridEvent.data.authorization.evidence.principalId).DisplayName
+        if ($null -eq $Requestor) {
             Write-Host "MSI may not have permission to read the applications from the directory"
-            $caller = $eventGridEvent.data.authorization.evidence.principalId
+            $Requestor = $eventGridEvent.data.authorization.evidence.principalId
         }
     }
 }
 
 # Write-Host "Authorization Action: $($eventGridEvent.data.authorization.action)"
 Write-Host "Authorization Scope: $($eventGridEvent.data.authorization.scope)"
-Write-Host "Operation Name: $lastOperation"
-Write-Host "Caller: $caller"
+Write-Host "Operation Name: $Action"
+Write-Host "Caller: $Requestor"
 $resourceId = $eventGridEvent.data.resourceUri
 Write-Host "ResourceId: $resourceId"
 Write-Host "Human Readable Action: $($eventGridEvent.data.operationName.localizedValue)"
 
-if (($null -eq $caller) -or ($null -eq $resourceId)) {
+if (($null -eq $Requestor) -or ($null -eq $resourceId)) {
     Write-Host "ResourceId or Caller is null"
     exit;
 }
 
-$ignore = @("providers/Microsoft.Resources/deployments", "providers/Microsoft.Resources/tags")
+$ignore = @("providers/Microsoft.Resources/deployments", "providers/Microsoft.Resources/tags", "Microsoft.Resources/tags/write")
 
 foreach ($case in $ignore) {
-    if ($resourceId -match $case) {
-        Write-Host "Skipping event as resourceId contains: $case"
+    if ($Action -match $case) {
+        Write-Host "Skipping the event matching the case $case"
         exit;
     }
 }
 
 # # Get first taggable resource
-# $resourceId = Get-ParentResourceId -ResourceId $resourceId
-# Write-Host "!!!! Found parent resource: $resourceId"
+$resourceId = Get-ParentResourceId -ResourceId $resourceId
+Write-Host "!!!! Found parent resource: $resourceId"
 
 $tags = (Get-AzTag -ResourceId $resourceId).Properties
 
 if (!($tags.TagsProperty.ContainsKey('CreatedBy')) -or ($null -eq $tags)) {
     $tag = @{
-        CreatedBy = $caller;
+        CreatedBy = $Requestor;
         CreatedDate = $(Get-Date);
-        LastOperation = $lastOperation;
+        LastOperation = $Action;
     }
     Update-AzTag -ResourceId $resourceId -Operation Merge -Tag $tag
-    Write-Host "Added CreatedBy tag with user: $caller"
+    Write-Host "Added CreatedBy tag with user: $Requestor"
 }
 else {
     Write-Host "Tag already exists"
-    # $tag = @{
-    #     LastModifiedBy = $caller;
-    #     LastModifiedDate = $(Get-Date);
-    #     LastOperation = $lastOperation;
-    # }
-    # Update-AzTag -ResourceId $resourceId -Operation Merge -Tag $tag
-    # Write-Host "Added or updated ModifiedBy tag with user: $caller"
+    $tag = @{
+        LastModifiedBy = $Requestor;
+        LastModifiedDate = $(Get-Date);
+        LastOperation = $Action;
+    }
+    Update-AzTag -ResourceId $resourceId -Operation Merge -Tag $tag
+    Write-Host "Added or updated ModifiedBy tag with user: $Requestor"
 }
