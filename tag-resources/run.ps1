0d5570eb093fe04145bd22e85a5646d938f26c8b
Justin OConnor <jsoconno@gmail.com>
2021-10-06 16:25:43 -0400

    update logic

--- tag-resources/run.ps1
+++ tag-resources/run.ps1
@@ -113,7 +113,7 @@ foreach ($case in $ignore) {
 
 # # Get first taggable resource
 # $resourceId = Get-ParentResourceId -ResourceId $resourceId
-$resourceId = $(Get-ParentResourceId -ResourceId $resourceId).id
+$resourceId = $(Get-ParentResourceId -ResourceId $resourceId).id.replace("/providers/Microsoft.Resources/tags/default", "")
 Write-Host "Attempting to tag $($resourceId)"
 
 $tags = (Get-AzTag -ResourceId $resourceId).Properties
