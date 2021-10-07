e1511829018d0f8e70db3886fd8a4b119d74c6dd
Justin OConnor <jsoconno@gmail.com>
2021-10-07 10:10:48 -0400

    clean up code and add event timestamp attribute for testing

--- tag-resources/run.ps1
+++ tag-resources/run.ps1
@@ -1,107 +1,117 @@
 param($eventGridEvent, $TriggerMetadata)
 
 function Get-ParentResourceId {
+
     param(
-        $ResourceID
+        $ResourceId
     )
 
-    $ResourceIDList = $ResourceID -Split '/'
+    $ResourceIdList = $ResourceId -Split '/'
     $IgnoreList = @('subscriptions', 'resourceGroups', 'providers')
 
-    for ($ia=$ResourceIDList.length-1; $ia -ge 0; $ia--) {
-        $CurrentResourceIDList = $ResourceIDList[0..($ia)]
-        $CurrentResourceID = $CurrentResourceIDList -Join '/'
-        $CurrentHead = $CurrentResourceIDList[-1]
+    for ($ia=$ResourceIdList.length-1; $ia -ge 0; $ia--) {
+        $CurrentResourceIdList = $ResourceIdList[0..($ia)]
+        $CurrentResourceId = $CurrentResourceIdList -Join '/'
+        $CurrentHead = $CurrentResourceIdList[-1]
         if (!($IgnoreList -Contains $CurrentHead)) {
-            Write-Host "Validating ability to tag $($CurrentResourceID)" 
-            $Error.clear()
+            Write-Host "Validating ability to tag $($CurrentResourceId)" 
+            $Error.Clear()
             try {
                 try {
                     Write-Host "Running tagging test..."
-                    Get-AzTag -ResourceId $CurrentResourceID
+                    Get-AzTag -ResourceId $CurrentResourceId
                 } catch {
                     Write-Host "Test failed." -ForegroundColor Red
                 }
             } catch {
                 Throw $_.Exception
-                Write-Host "$($CurrentResourceID) cannot be tagged.  Searching for parent."
+                Write-Host "$($CurrentResourceId) cannot be tagged.  Searching for parent."
             }
             if (!$Error) {
                 Write-Host "Test passed." -ForegroundColor Green
                 Break
             }
         } else {
-            Write-Host "Skipping resource $($CurrentResourceID)."
+            Write-Host "Skipping resource $($CurrentResourceId)."
         }
     }
 
-    Return $CurrentResourceID
+    Return $CurrentResourceId
 }
 
-$Requestor = $eventGridEvent.data.claims.name
+function Get-Requestor {
+
+    param(
+        $Requestor
+    )
+
+    if ($null -eq $Requestor) {
+        if ($eventGridEvent.data.authorization.evidence.principalType -eq "ServicePrincipal") {
+            $PrincipalId = $eventGridEvent.data.authorization.evidence.principalId
+            $Requestor = (Get-AzADServicePrincipal -ObjectId $PrincipalId).DisplayName
+            if ($null -eq $Requestor) {
+                Write-Host "The identity does not have permission read the application from the directory."
+                $Requestor = $PrincipalId
+            }
+        }
+    }
+
+    Return $Requestor
+}
+
+$Requestor = Get-Requestor -Requestor $eventGridEvent.data.claims.name
 $Action = $eventGridEvent.data.authorization.action
+$AuthorizationScope = $eventGridEvent.data.authorization.scope # $eventGridEvent.data.resourceUri
+$EventTimestamp = $eventGridEvent.data.$eventTimestamp
 
-if ($null -eq $Requestor) {
-    if ($eventGridEvent.data.authorization.evidence.principalType -eq "ServicePrincipal") {
-        $Requestor = (Get-AzADServicePrincipal -ObjectId $eventGridEvent.data.authorization.evidence.principalId).DisplayName
-        if ($null -eq $Requestor) {
-            Write-Host "MSI may not have permission to read the applications from the directory"
-            $Requestor = $eventGridEvent.data.authorization.evidence.principalId
-        }
-    }
-}
+Write-Host "Authorization Scope: $($AuthorizationScope)"
+Write-Host "Event Timestamp: $($EventTimestamp)"
 
-# Write-Host "Authorization Action: $($eventGridEvent.data.authorization.action)"
-Write-Host "Authorization Scope: $($eventGridEvent.data.authorization.scope)"
-Write-Host "Operation Name: $Action"
-Write-Host "Caller: $Requestor"
-$resourceId = $eventGridEvent.data.authorization.scope # $eventGridEvent.data.resourceUri
-Write-Host "ResourceId: $resourceId"
-Write-Host "Human Readable Action: $($eventGridEvent.data.operationName.localizedValue)"
+$ResourceId = $(Get-ParentResourceId -ResourceId $AuthorizationScope).id
+Write-Host "Initial Resource ID: $($ResourceId)"
+$ResourceId = $ResourceId.replace("/providers/Microsoft.Resources/tags/default", "")
+$ResourceId = $ResourceId.replace("/blobServices/default", "")
+Write-Host "Clean Resource ID: $($ResourceId)"
 
-if (($null -eq $Requestor) -or ($null -eq $resourceId)) {
-    Write-Host "ResourceId or Caller is null"
+Write-Host "Operation Name Properties: $($eventGridEvent.data.operationName.Properties)"
+
+if (($null -eq $Requestor) -or ($null -eq $ResourceId)) {
+    Write-Host "Requestor or Resource ID is null."
     exit;
 }
 
 # cean this up
-$ignore = @("providers/Microsoft.Resources/deployments", "providers/Microsoft.Resources/tags", "Microsoft.Resources/tags/write", "Microsoft.Authorization/policies/auditIfNotExists/action")
+$Ignore = @("providers/Microsoft.Resources/deployments", "providers/Microsoft.Resources/tags", "Microsoft.Resources/tags/write", "Microsoft.Authorization/policies/auditIfNotExists/action")
 
-foreach ($case in $ignore) {
-    if ($Action -match $case) {
-        Write-Host "Skipping the event matching the case $case"
+foreach ($Case in $Ignore) {
+    if ($Action -match $Case) {
+        Write-Host "Skipping the event matching the case $Case"
         exit;
     }
 }
 
-# # Get first taggable resource
-# $resourceId = Get-ParentResourceId -ResourceId $resourceId
-$baseResourceId = $(Get-ParentResourceId -ResourceId $resourceId).id
-$cleanPostfixResourceId = $baseResourceId.replace("/providers/Microsoft.Resources/tags/default", "")
-$resourceId = $cleanPostfixResourceId.replace("/blobServices/default", "")
-# $resourceId = $(Get-ParentResourceId -ResourceId $resourceId).id.replace("/providers/Microsoft.Resources/tags/default", "").replace("/blobServices/default", "")
-Write-Host "Attempting to tag $($resourceId)"
+Write-Host "Attempting to tag $($ResourceId)"
 
-$tags = (Get-AzTag -ResourceId $resourceId).Properties
+$Tags = (Get-AzTag -ResourceId $ResourceId).Properties
 
-Write-Host $tags
+Write-Host $Tags
 
-if (!($tags.TagsProperty.ContainsKey('CreatedBy')) -or ($null -eq $tags)) {
+if (!($Tags.TagsProperty.ContainsKey('CreatedBy')) -or ($null -eq $Tags)) {
     Write-Host "Adding CreatedBy tags."
-    $tag = @{
+    $Tag = @{
         CreatedBy = $Requestor;
         CreatedDate = $(Get-Date);
         LastOperation = $Action;
     }
-    Update-AzTag -ResourceId $resourceId -Operation Merge -Tag $tag
+    Update-AzTag -ResourceId $ResourceId -Operation Merge -Tag $Tag
     Write-Host "Added CreatedBy tag with user: $Requestor"
 } else {
     Write-Host "Adding ModifiedBy tags."
-    $tag = @{
+    $Tag = @{
         LastModifiedBy = $Requestor;
         LastModifiedDate = $(Get-Date);
         LastOperation = $Action;
     }
-    Update-AzTag -ResourceId $resourceId -Operation Merge -Tag $tag
+    Update-AzTag -ResourceId $ResourceId -Operation Merge -Tag $Tag
     Write-Host "Added or updated ModifiedBy tag with user: $Requestor"
 }
